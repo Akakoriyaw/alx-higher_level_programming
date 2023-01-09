1fb176dffb498084f40c02483ad3f577bf437a50
Akakoriyaw <akakoriyaw@gmail.com>
2023-01-09 13:54:01 -0800

    fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""class Rectangle module - no test cases needed"""
-Rectangle = __import__('9-rectangle').Rectangle
+"""Square class Module"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """is a sublass of Rectangle"""
+    """Square class"""
     def __init__(self, size):
-        """init method"""
+        """Method for initializing a square"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """get the area of square"""
-        return self.__size * self.
+        """Method that returns area of a square"""
+        return self.__size ** 2
